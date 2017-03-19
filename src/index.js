39015c5c938b5b23f53d071d00577ac4c2b770a1
JudyVue <judyvue87@gmail.com>
2017-03-19 15:48:02 -0700

    indented component names in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import ytSearch from 'youtube-api-search';
+import React, { Component } from 'react'; //dep
+import ReactDOM from 'react-dom'; //dep
+import ytSearch from 'youtube-api-search'; //dep
 import SearchBar from './components/search-bar';
 import VideoList from './components/video-list';
 
@@ -9,6 +9,7 @@ const API_KEY = 'AIzaSyA5iBAoKE4IXylbiskEqHOdFX8uxMfQUik';
 //create a new component. This component should produce some HTML
 class App extends Component {
   constructor(props) {
+    //The super keyword is used to call functions on an object's parent.
     super(props);
 
     this.state = { videos: []};
@@ -22,8 +23,8 @@ class App extends Component {
   render(){
     return (
       <div>
-      <SearchBar />
-      <VideoList videos={this.state.videos} />
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
