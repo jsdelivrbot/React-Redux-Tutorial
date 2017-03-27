b9e19cb25e470706b7ea4957037bd1e1f87a9362
JudyVue <judyvue87@gmail.com>
2017-03-26 18:50:44 -0700

    added more code to video-detail

--- src/components/video-list-item.js
+++ src/components/video-list-item.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-  return <li>Video</li>
-}
+//{video} is ES6 syntax to just grab video property from props object
+const VideoListItem = ({video}) => {
+  const imageURL = video.snippet.thumbnails.default.url;
+  return(
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={imageURL}/>
+        </div>
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
+    </li>
+  );
+};
 
 export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'; //dep
 import ytSearch from 'youtube-api-search'; //dep
 import SearchBar from './components/search-bar';
 import VideoList from './components/video-list';
+import VideoDetail from './components/video-detail';
 
 const API_KEY = 'AIzaSyA5iBAoKE4IXylbiskEqHOdFX8uxMfQUik';
 
@@ -24,6 +25,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
         <VideoList videos={this.state.videos} />
       </div>
     );
